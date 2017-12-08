dd0c52db3aff7a3347e78417c7dfa1365b0594d4
Jacob Silver <jacobsilver2@mac.com>
2017-12-08 16:12:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ function moveDodgerRight() {
  function removeElementsByClass(className){
    var elements = document.getElementsByClassName(className);
    for (var i = 0; i<elements.length; i++){
-       elements[0].parentNode.removeChild(elements[0]);
+       elements.splice[0];
    }
 }
 
