8de5c3047bdc0457ecbf620529c84692acb411cd
Jacob Silver <jacobsilver2@mac.com>
2017-12-08 14:40:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -145,6 +145,7 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    if (e.which === LEFT_ARROW) {
+     e.preventDefault();
      moveDodgerLeft();
    }
    if (e.which === RIGHT_ARROW) {
